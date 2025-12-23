4132ec839c1ef45de47543de3b9443bb199a48d4
ADARSH U K <adarshuk@Mygates-MacBook-Pro.local>
2025-12-23 09:24:17 +0530

    Day 2:BaseTest and framework skeleton

---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,11 @@
+package base;
+
+public class BaseTest {
+
+    public void setUp(){
+        System.out.println("This is from setup");
+    }
+    public void tearDown(){
+        System.out.println("This is from tearDown");
+    }
+}
---
+++ src/test/java/tests/SampleTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import base.BaseTest;
+
+public class SampleTest extends BaseTest {
+
+    public void testExample(){
+        setUp();
+        System.out.println("This is from testExample");
+        tearDown();
+    }
+
+}
